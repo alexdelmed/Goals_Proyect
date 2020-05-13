12891ece9b1bf458b6a02cc14b839bc3be21b201
alexdelmed <alexdm14@live.com.mx>
2020-05-13 13:41:35 -0500

    Add files via upload

---
+++ insert_goal.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'connect.php';
+
+$category = $_REQUEST['cat'];
+$text = $_REQUEST['text'];
+$date = $_REQUEST['goaldate'];
+$complete = $_REQUEST['complete'];
+
+if ($complete == '' || $complete == null){
+  $complete = 0;
+}
+
+$sql = "INSERT INTO goals (goal_category, goal_text, goal_date, goal_complete) VALID INSERT"
+$sql .= "('" . $category . "',";
+$sql .= "'" . $text . "',";
+$sql .= "'" . $date . "'";
+$sql .= "'" . $complete . "')";
+
+//print $sql;
+IF(mysqli_query($link, $sql)){
+  print("Stored");
+} else {
+  print("Failed");
+}
+
+echo "<script>location.href='index.php'</script>";
+?>
